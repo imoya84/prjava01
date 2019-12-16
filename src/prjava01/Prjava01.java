a933da00e0d2184c5536fe437c8d7dbf77348351
Ivan <Ivan@DESKTOP-H27R5DC>
2019-12-16 22:13:18 +0100

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -11,7 +11,7 @@ public class Prjava01 {
      */
     public static void main(String[] args) throws IOException {
         File f = new File("fitxer.html");
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {            
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
@@ -19,6 +19,7 @@ public class Prjava01 {
 		bw.write("    </title>");bw.newLine();
 		bw.write("  </head>");bw.newLine();
 		bw.write("  <body>");bw.newLine();
+                bw.write(" <h1>DAW2 m08uf4eac1</h1>");bw.newLine(); 
 		bw.write("    Nova p&agrave;gina web");bw.newLine();
 		bw.write("  </body>");bw.newLine();
 		bw.write("</html>");bw.newLine();
